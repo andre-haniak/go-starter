dd0e611e795fa78bdd196e10b5edf56613d33d13
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-04-10 11:42:01 -0300

    define methods for user models

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/andre-haniak/go-starter/models"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	user := models.User{
+		ID:        1,
+		FirstName: "Andre",
+		LastName:  "Haniak",
+	}
+	fmt.Println(user)
 }
